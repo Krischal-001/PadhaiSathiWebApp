836fe5aeac12a0d44761c7888c403275a4e3931f
Nitish <nitishstha15@gmail.com>
2026-07-08 21:12:02 +0545

    Update auth

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 
 const AuthContext = createContext(null);
 const API = import.meta.env.VITE_API_URL;
-
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
