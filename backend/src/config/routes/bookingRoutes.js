8c5be196ba2354fd806b5c8913872654acd0a737
Nitish <nitishstha15@gmail.com>
2026-07-12 14:25:18 +0545

    Initial commit

--- backend/src/config/routes/bookingRoutes.js
+++ backend/src/config/routes/bookingRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 const {
+  
   createBooking,
   getMyBookings,
   getBookingById,
